ff88299664961dc40677b16af16cc1802a325549
Nicholas Hooley <nicholashooley27@yahoo.co.uk>
2022-07-20 17:29:50 +0100

    introduced using query params. reading query params to change button text. need to implement list sorting

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,11 +1,33 @@
 import { Fragment } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
 
 const QuoteList = (props) => {
+  const history = useHistory();
+
+  // allows us to get the query param data
+  const location = useLocation();
+
+  const queryparams = new URLSearchParams(location.search);
+
+  const isSortingAscending = queryparams.get('sort') === 'asc';
+
+  const changeSortingHandler = () => {
+    // basically changing the url to add query params
+    // push causes a rerender
+    history.push('/quotes?sort=' + (isSortingAscending ? 'desc' : 'asc'));
+    console.log(location);
+  };
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSortingHandler}>
+          Sort {isSortingAscending ? 'Descending' : 'Ascending '}
+        </button>
+      </div>
       <ul className={classes.list}>
         {props.quotes.map((quote) => (
           <QuoteItem
